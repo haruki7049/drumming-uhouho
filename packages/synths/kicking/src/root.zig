85b0111ac6b2578c416217d18a0d0c087472f0df
haruki7049 <tontonkirikiri@gmail.com>
2026-02-12 16:04:57 +0900

    Create kicking synth

---
+++ packages/synths/kicking/src/root.zig
@@ -0,0 +1,84 @@
+const std = @import("std");
+const lightmix = @import("lightmix");
+const lightmix_filters = @import("lightmix_filters");
+const lightmix_synths = @import("lightmix_synths");
+
+const Wave = lightmix.Wave;
+const cutAttack = lightmix_filters.volume.cutAttack;
+const CutAttackArgs = lightmix_filters.volume.CutAttackArgs;
+const decay = lightmix_filters.volume.decay;
+const DecayArgs = lightmix_filters.volume.DecayArgs;
+
+pub fn gen(comptime T: type, options: Options) !Wave(T) {
+    var drum: Wave(f128) = try lightmix_synths.Basic.Sine.gen(f128, .{
+        .frequency = options.frequency,
+        .length = options.length,
+        .amplitude = options.amplitude * 2.0,
+        .allocator = options.allocator,
+        .sample_rate = options.sample_rate,
+        .channels = options.channels,
+    });
+
+    for (0..1) |_| {
+        try drum.filter_with(CutAttackArgs, cutAttack, .{});
+    }
+    for (0..12) |_| {
+        try drum.filter_with(DecayArgs, decay, .{});
+    }
+    for (0..1) |_| {
+        try drum.filter(normalize);
+    }
+
+    return drum;
+}
+
+pub const Options = struct {
+    allocator: std.mem.Allocator,
+    frequency: f32,
+    length: usize,
+    amplitude: f32,
+    sample_rate: u32,
+    channels: u16,
+};
+
+pub fn testwave_gen() !Wave(f128) {
+    const allocator = std.heap.page_allocator;
+
+    var result: Wave(f128) = try gen(f128, .{
+        .allocator = allocator,
+        .frequency = 55.0,
+        .length = 44100,
+        .amplitude = 1.0,
+        .sample_rate = 44100,
+        .channels = 1,
+    });
+    try result.filter(normalize);
+
+    return result;
+}
+
+fn normalize(comptime T: type, original_wave: Wave(T)) !Wave(T) {
+    const allocator = original_wave.allocator;
+    var result: std.array_list.Aligned(T, null) = .empty;
+
+    var max_volume: T = 0.0;
+    for (original_wave.samples) |sample| {
+        if (@abs(sample) > max_volume)
+            max_volume = @abs(sample);
+    }
+
+    for (original_wave.samples) |sample| {
+        const volume: T = 1.0 / max_volume;
+
+        const new_sample: T = sample * volume;
+        try result.append(allocator, new_sample);
+    }
+
+    return Wave(T){
+        .samples = try result.toOwnedSlice(allocator),
+        .allocator = allocator,
+
+        .sample_rate = original_wave.sample_rate,
+        .channels = original_wave.channels,
+    };
+}
